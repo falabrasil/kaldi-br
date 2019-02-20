033a69b03fb2088950ec27b67b83685573209274
Cassio Batista <cassio.batista.13@gmail.com>
2019-02-20 17:28:29 -0300

    check_*.sh script now tells you which and how many files have duplicate names

--- check_wavnames.sh
+++ check_wavnames.sh
@@ -7,7 +7,18 @@ then
 fi
 
 find $1 -name "*.wav" > wavlist.orig
-cat wavlist.orig | sed 's/\// /g' | awk '{print $NF}' > wavlist.uniq
+cat wavlist.orig | sed 's/\// /g' | awk '{print $NF}' | sort | uniq > wavlist.uniq
 
-wc -l wavlist.*
-rm wavlist.*
+o=$(wc -l wavlist.orig | awk '{print $1}')
+u=$(wc -l wavlist.uniq | awk '{print $1}')
+
+if [[ $o == $u ]] ; then
+	echo "your audio corpora do not appear to have common filenames"
+	echo "original wavlist: ${o} files"
+	echo "unique wavlist:   ${u} files"
+else
+	echo "WE HAVE A PROBLEM WITH THESE FILES: "
+	cat wavlist.orig | sed 's/\// /g' | awk '{print $NF}' | sort | uniq -cd 
+fi
+
+rm -f wavlist.*
