24075612162fd6ce50619a002ec8881c3c895882
larissadias <larissa.engcomp@gmail.com>
2019-09-02 21:57:38 -0300

    Source onlinebin in path.sh

--- fb_00_create_envtree.sh
+++ fb_00_create_envtree.sh
@@ -105,7 +105,7 @@ echo \
 export KALDI_ROOT=$KALDI_ROOT # TODO: check correctness -- CB
 
 # Setting paths to useful tools 
-export PATH=\$PWD/utils/:\$KALDI_ROOT/src/bin:\$KALDI_ROOT/tools/openfst/bin:\$KALDI_ROOT/src/fstbin/:\$KALDI_ROOT/src/gmmbin/:\$KALDI_ROOT/src/featbin/:\$KALDI_ROOT/src/lmbin/:\$KALDI_ROOT/src/sgmm2bin/:\$KALDI_ROOT/src/fgmmbin/:\$KALDI_ROOT/src/latbin/:\$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/ivectorbin:$KALDI_ROOT/src/online2bin:\$PWD:\$PATH
+export PATH=\$PWD/utils/:\$KALDI_ROOT/src/bin:\$KALDI_ROOT/tools/openfst/bin:\$KALDI_ROOT/src/fstbin/:\$KALDI_ROOT/src/gmmbin/:\$KALDI_ROOT/src/featbin/:\$KALDI_ROOT/src/lmbin/:\$KALDI_ROOT/src/sgmm2bin/:\$KALDI_ROOT/src/fgmmbin/:\$KALDI_ROOT/src/latbin/:\$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/ivectorbin:$KALDI_ROOT/src/onlinebin:$KALDI_ROOT/src/online2bin:\$PWD:\$PATH
 
 # Defining audio data directory (TODO modify it for your installation directory!)
 export DATA_ROOT=\"/home/{user}/kaldi-trunk/egs/digits/digits_audio\"
