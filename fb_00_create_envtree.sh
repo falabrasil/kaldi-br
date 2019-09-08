76f80e01311b57a9a19ca90f39283b7fa9572ec9
Cassio Batista <cassio.batista.13@gmail.com>
2019-09-08 13:56:09 -0300

    [fb_00] TODO added to path.sh

--- fb_00_create_envtree.sh
+++ fb_00_create_envtree.sh
@@ -111,7 +111,7 @@ export KALDI_ROOT=$KALDI_ROOT # TODO: check correctness -- CB
 export PATH=\$PWD/utils/:\$KALDI_ROOT/src/bin:\$KALDI_ROOT/tools/openfst/bin:\$KALDI_ROOT/src/fstbin/:\$KALDI_ROOT/src/gmmbin/:\$KALDI_ROOT/src/featbin/:\$KALDI_ROOT/src/lmbin/:\$KALDI_ROOT/src/sgmm2bin/:\$KALDI_ROOT/src/fgmmbin/:\$KALDI_ROOT/src/latbin/:\$KALDI_ROOT/src/nnet2bin/:$KALDI_ROOT/src/ivectorbin:$KALDI_ROOT/src/onlinebin:$KALDI_ROOT/src/online2bin:\$PWD:\$PATH
 
 # Defining audio data directory (TODO modify it for your installation directory!)
-export DATA_ROOT=\"/home/{user}/kaldi-trunk/egs/digits/digits_audio\"
+export DATA_ROOT=\"/home/{user}/kaldi-trunk/egs/digits/digits_audio\" # TODO: mustn't this be checked? -- CB
 
 # Enable SRILM 
 . \$KALDI_ROOT/tools/env.sh
