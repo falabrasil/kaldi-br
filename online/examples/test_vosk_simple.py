1d39f804828a72d79a351f9ad49dc81110462b6b
Cassio Batista <cassio.batista.13@gmail.com>
2020-11-22 22:00:56 -0300

    [online] pass model dir by argv

--- online/examples/test_vosk_simple.py
+++ online/examples/test_vosk_simple.py
@@ -11,12 +11,12 @@ import wave
 #    print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
 #    exit (1)
 
-wf = wave.open(sys.argv[1], "rb")
+wf = wave.open(sys.argv[2], "rb")
 if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
     print ("Audio file must be WAV format mono PCM.")
     exit (1)
 
-model = Model("/home/cassio/vosk-model")
+model = Model(sys.argv[1])
 rec = KaldiRecognizer(model, wf.getframerate())
 
 while True:
