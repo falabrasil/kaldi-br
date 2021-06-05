8d7de443c60460d601e54adbf8d9d54540d0af17
Cassio Batista <cassio.batista.13@gmail.com>
2021-06-05 04:42:33 -0300

    [utils] add functional decode_files.sh script

--- utils/online/decode_files.sh
+++ utils/online/decode_files.sh
@@ -17,10 +17,13 @@
 
 set -e
 
-stage=0
+function msg { echo -e "\e[$(shuf -i 91-96 -n 1)m[$(date +'%F %T')] $1\e[0m" ; }
+
+stage=1
 data=data/decodeme
 
-am_dir=exp/chain_online_cmn/tdnn
+am_dir=exp/chain_online_cmn/tdnn1k_sp
+ie_dir=exp/nnet3_online_cmn/extractor
 
 lm_small_file=
 lm_large_file=
@@ -45,34 +48,32 @@ fi
 audio_files=$@
 
 # sanity check
-[ ! -d $am_dir ] && echo "$0: error: dir '$am_dir' must exist" && exit 1
-for f in $lm_small_file $lex_file ${audio_files[@]} ; do
-  [ ! -f $f ] && echo "$0: error: file '$f' must exist" && exit 1
-done
+for d in $am_dir $ie_dir ; do [ ! -d $d ] && echo "$0: error: dir '$d' must exist" && exit 1 ; done
+for f in $lm_small_file $lex_file ${audio_files[@]} ; do [ ! -f $f ] && echo "$0: error: file '$f' must exist" && exit 1 ; done
 [ -z $lm_large_file ] && echo "$0: WARNING: high-order LM not set. Lattice rescoring will NOT be performed"
 
 mkdir -p $data/data
 mkdir -p $data/local/{dict,lm}
 
+cp -v data/local/dict/{optional_silence,silence_phones,nonsilence_phones,extra_questions}.txt $data/local/dict
+
 # data prep
 # set number of jobs equal to the number of input audio files
 nj=0
-if [ $stage -le 0 ] ; then
-  msg "$0: prepare data"
-  rm -f $data/data/{wav.scp,utt2spk}
-  for wav in ${audio_files[@]} ; do
-    nj=$((nj + 1))
-    utt_id=$(basename ${wav%.wav})
-    echo "$utt_id $(readlink -e $wav)" >> $data/data/wav.scp
-    echo "$utt_id $utt_id" >> $data/data/utt2spk
-  done
-  utils/utt2spk_to_spk2utt $data/data/utt2spk > $data/data/spk2utt
-  utils/validate_data_dir.sh --non-print --no-feats --no-text $data/data
+msg "$0: prepare data"
+rm -f $data/data/{wav.scp,utt2spk}
+for wav in ${audio_files[@]} ; do
+  nj=$((nj + 1))
+  utt_id=$(basename ${wav%.wav})
+  echo "$utt_id $(readlink -e $wav)" >> $data/data/wav.scp
+  echo "$utt_id $utt_id" >> $data/data/utt2spk
+done
+utils/utt2spk_to_spk2utt.pl $data/data/utt2spk > $data/data/spk2utt
+utils/validate_data_dir.sh --non-print --no-feats --no-text $data/data
 
-  ln -rsvf $lex_file      $data/local/dict/lexicon.txt
-  ln -rsvf $lm_small_file $data/local/lm/small.arpa
-  ln -rsvf $lm_large_file $data/local/lm/large.arpa
-fi
+ln -rsvf $lex_file      $data/local/dict/lexicon.txt
+ln -rsvf $lm_small_file $data/local/lm/small.arpa
+ln -rsvf $lm_large_file $data/local/lm/large.arpa
 
 # lang prep
 if [ $stage -le 1 ] ; then
@@ -104,17 +105,17 @@ fi
 if [ $stage -le 3 ] ; then
   msg "$0: mkgraph"
   /usr/bin/time -f "mkgraph took %U secs.\tRAM: %M KB" \
-    steps/mkgraph.sh --self-loop-scale 1.0 \
-      $data/lang $data/nnet3 $data/nnet3/graph
+    utils/mkgraph.sh --self-loop-scale 1.0 \
+      $data/lang $am_dir $data/tdnn/graph
 fi
 
 # prep online decode
 if [ $stage -le 4 ] ; then
   msg "$0: prepare online decoding"
-  steps/online/nnet3/prepapre_online_decoding.sh \
-    --mfcc-config $data/nnet3/conf/mfcc.conf \
-    --online-cmvn-config $data/nnet3/conf/online_cmvn.conf \
-    $data/lang $data/nnet3/ivector_extractor $data/nnet3 $data/online
+  steps/online/nnet3/prepare_online_decoding.sh \
+    --mfcc-config conf/mfcc_hires.conf \
+    --online-cmvn-config $ie_dir/online_cmvn.conf \
+    $data/lang $ie_dir $am_dir $data/online
 fi
 
 # decode
@@ -123,7 +124,7 @@ if [ $stage -le 5 ] ; then
   /usr/bin/time -f "decoding took %U secs.\tRAM: %M KB" \
     steps/online/nnet3/decode.sh --nj $nj --cmd "$decode_cmd" --skip-scoring true \
       --acwt 1.0 --post-decode-acwt 10.0 --lattice-beam 8.0 --per-utt true \
-      $data/nnet3/graph $data/data $data/online/decode_small
+      $data/tdnn/graph $data/data $data/online/decode_small
 fi
 
 # rescore (extracted from steps/lmrescore_const_arpa.sh)
@@ -131,11 +132,11 @@ if [ ! -z $lm_large_file ] && [ $stage -le 6 ] ; then
   rm -rf $data/online/decode_large/log
   mkdir -p $data/online/decode_large/log
   msg "$0: rescoring lattices (2nd pass, large LM)"
-  old_ark_in="ark:gunzip -c $data/online/decode_small/lat.JOB.gz | fstproject --project_output=true $data/lang/G.fst |"
+  old_ark_in="ark:gunzip -c $data/online/decode_small/lat.JOB.gz |"
   new_ark_out="ark,t:|gzip -c > $data/online/decode_large/lat.JOB.gz"
   /usr/bin/time -f "lattice rescoring took %U secs.\tRAM: %M KB" \
     run.pl JOB=1:$nj $data/online/decode_large/log/rescorelm.JOB.log \
-      lattice-lmrescore --lm-scale=-1.0 "$old_ark_in" ark:- \| \
+      lattice-lmrescore --lm-scale=-1.0 "$old_ark_in" "fstproject --project_output=true $data/lang/G.fst |" ark:- \| \
       lattice-lmrescore-const-arpa --lm-scale=1.0 ark:- $data/lang/G.carpa "$new_ark_out"
 fi
 
