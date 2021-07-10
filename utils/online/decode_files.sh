723b56fa68c1ec82bd33099cb9f9114676fbfd4e
Cassio Batista <cassio.batista.13@gmail.com>
2021-07-09 22:18:33 -0300

    [utils] online/decode_files.sh: sort entries to wav.scp and utt2spk

--- utils/online/decode_files.sh
+++ utils/online/decode_files.sh
@@ -25,14 +25,13 @@ function msg { echo -e "\e[$(shuf -i 91-96 -n 1)m[$(date +'%F %T')] $1\e[0m" ; }
 
 stage=1
 nj=1
-with_confidence=false
+with_confidence=true
 data=data/decodeme
 
 am_dir=exp/chain_online_cmn/tdnn1k_sp
 ie_dir=exp/nnet3_online_cmn/extractor
 tree_dir=exp/chain_online_cmn/tree_sp
 
-. cmd.sh
 . path.sh
 . utils/parse_options.sh
 
@@ -72,6 +71,8 @@ for wav in ${audio_files[@]} ; do
   echo "$utt_id $(readlink -e $wav)" >> $data/data/wav.scp
   echo "$utt_id $utt_id" >> $data/data/utt2spk
 done
+sort -u $data/data/wav.scp -o $data/data/wav.scp
+sort -u $data/data/utt2spk -o $data/data/utt2spk
 utils/utt2spk_to_spk2utt.pl $data/data/utt2spk > $data/data/spk2utt
 utils/validate_data_dir.sh --non-print --no-feats --no-text $data/data
 
@@ -104,7 +105,7 @@ fi
 if [ $stage -le 3 ] ; then
   msg "$0: decoding (1st pass, small LM)"
   /usr/bin/time -f "decoding took %E.\tRAM: %M KB" \
-    steps/online/nnet3/decode.sh --nj $nj --cmd "$decode_cmd" --skip-scoring true \
+    steps/online/nnet3/decode.sh --nj $nj --skip-scoring true \
       --acwt 1.0 --post-decode-acwt 10.0 --per-utt true \
       --lattice-beam 6.0 --beam 10.0 --max-active 6000 \
       $tree_dir/graph_small $data/data $data/online/decode_small
@@ -115,21 +116,25 @@ if [ -f data/lang_test_large/G.carpa ] && [ $stage -le 4 ] ; then
   rm -rf   $data/online/decode_large/log
   mkdir -p $data/online/decode_large/log
   msg "$0: rescoring lattices (2nd pass, large LM)"
-  old_ark_in="ark:gunzip -c $data/online/decode_small/lat.JOB.gz |"
-  new_ark_out="ark,t:|gzip -c > $data/online/decode_large/lat.JOB.gz"
   /usr/bin/time -f "lattice rescoring took %E.\tRAM: %M KB" \
     run.pl JOB=1:$nj $data/online/decode_large/log/rescorelm.JOB.log \
-      lattice-lmrescore --lm-scale=-1.0 "$old_ark_in" "fstproject --project_output=true data/lang_test_small/G.fst |" ark:- \| \
-      lattice-lmrescore-const-arpa --lm-scale=1.0 ark:- data/lang_test_large/G.carpa "$new_ark_out"
+      lattice-lmrescore --lm-scale=-1.0 \
+        old_ark_in="ark:gunzip -c $data/online/decode_small/lat.JOB.gz |" \
+        "fstproject --project_output=true data/lang_test_small/G.fst |" \
+        ark:- \| \
+      lattice-lmrescore-const-arpa --lm-scale=1.0 \
+        ark:- \
+        data/lang_test_large/G.carpa \
+        "ark,t:|gzip -c > $data/online/decode_large/lat.JOB.gz"
 fi
 
 # score (extracted from local/score.sh)
-# NOTE fixing LM weight to 9.0 and disabling word insertion penalty by default
+# NOTE LM weight fixed to 9.0 and word insertion penalty disabled by default
 # FIXME this parallelization needs more testing; 4 identical files took 18s to 
 #       to be score in 4 threads while only 12s on a single thread.
 if [ $stage -le 5 ] ; then
-  dir=$data/online/decode_small
-  [ -f data/lang_test_large/G.carpa ] && dir=$data/online/decode_large
+  dir=$data/online/decode_large
+  [ ! -f data/lang_test_large/G.carpa ] && dir=$data/online/decode_small
   rm -rf   $dir/scoring/log score.*
   mkdir -p $dir/scoring/log
   if $with_confidence ; then
@@ -141,6 +146,7 @@ if [ $stage -le 5 ] ; then
         lattice-to-ctm-conf --inv-acoustic-scale=9.0 --frame-shift="0.03" ark:- - \| \
         utils/int2sym.pl -f 5 data/lang/words.txt ">" score.ctm
   else
+    # FIXME output is *not* a CTM file here
     msg "$0: scoring lattice to get the 1-best hypothesis (no confidence score)"
     /usr/bin/time -f "scoring took %E.\tRAM: %M KB" \
       run.pl JOB=1:$nj $dir/scoring/log/best_path.JOB.log \
@@ -156,7 +162,7 @@ e_time=$(LC_ALL=C date)
 f=score.ctm
 [ -f $f ] && head -n 5 $f && echo "..." && tail -n 5 $f && \
   msg "$0: success! results on file '$f'" || \
-  echo "$0: file '$f' should've been created..."
+  echo "$0: WARNING: results file '$f' should've been created..."
 
 echo $s_time
 echo $e_time
